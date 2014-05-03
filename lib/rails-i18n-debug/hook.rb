e6996018882013b38758a780271dd110bd7aeb91
XeeD <xeedus@gmail.com>
2014-05-03 17:52:53 +0200

    If I18nLogger is defined use it instead of default Rails logger It could be defined for instance in config/environments/development.rb

--- lib/rails-i18n-debug/hook.rb
+++ lib/rails-i18n-debug/hook.rb
@@ -5,7 +5,7 @@ module I18n
         init_translations unless initialized?
         keys = I18n.normalize_keys(locale, key, scope, options[:separator])
 
-        Rails.logger.debug "I18N keys: #{keys}" if Rails.env.development?
+        i18n_logger.debug "I18N keys: #{keys}" if Rails.env.development?
 
         keys.inject(translations) do |result, _key|
           _key = _key.to_sym
@@ -13,10 +13,17 @@ module I18n
           result = result[_key]
           result = resolve(locale, _key, result, options.merge(:scope => nil)) if result.is_a?(Symbol)
 
-          Rails.logger.debug "\t\t => " + result.to_s + "\n" if Rails.env.development? && (result.class == String)
+          i18n_logger.debug "\t\t => " + result.to_s + "\n" if Rails.env.development? && (result.class == String)
 
           result
         end
+      end
+
+
+      private
+
+      def i18n_logger
+        defined?(I18nLogger) ? I18nLogger : Rails.logger
       end
     end
   end
